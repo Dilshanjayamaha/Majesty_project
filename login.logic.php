b608d4ca708e6e1031bcf4f9c338b7a3a458103a
hasithaea <hasitha2k4@gmail.com>
2026-01-25 03:25:16 +0530

    added login logic

---
+++ login.logic.php
@@ -0,0 +1,46 @@
+<?php
+    include "db.php";
+
+    session_start();
+
+    if(isset($_POST['submit']))
+        {
+           $email=$_POST['email'];
+           $password=$_POST['password']; 
+
+           $sql ="select * from users where email='$email'";
+           $result =mysqli_query($conn,$sql);
+           
+           if($result->num_rows>0)
+            {
+                $row =mysqli_fetch_assoc($result);
+                if($row['password'] == $password)
+                    {
+                        $_SESSION['user_id'] = $row['id'];
+                        $_SESSION['user_name'] = $row['name'];
+                        $_SESSION['user_role'] = $row['role'];
+                         
+                        if($_SESSION['user_role'] == "admin")
+                            {
+                                header("Location:dashboard.php");
+                            }
+
+                        else 
+                            {
+                                echo "dashboard for user";
+                            }
+                    }
+                
+                else
+                    {
+                        echo "Wrong Password";
+                    }
+
+            }
+            else
+                {
+                    echo "Please Go For Signup";
+                }
+        }
+
+?>
